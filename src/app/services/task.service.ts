23e2086f20df3b38ee130b70a40a04b6e3d2f8c1
SalvadorGalvez <salva01797@gmail.com>
2024-05-21 17:22:02 -0600

    Creacion del servicio TaskService

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Task } from '../models/task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  private readonly baseUrl: string = 'http://localhost:8080/api/tasks';
+
+  constructor(private http: HttpClient) { }
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.baseUrl);
+  }
+
+  getTask(id: number): Observable<Task> {
+    return this.http.get<Task>(`${this.baseUrl}/${id}`);
+  }
+
+  createTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.baseUrl, task);
+  }
+
+  updateTask(task: Task): Observable<Task> {
+    return this.http.put<Task>(`${this.baseUrl}/${task.id}`, task);
+  }
+
+  deleteTask(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
+}

b59dbf6cc3462e020d0379b833b019f8f5297dcf
SalvadorGalvez <salva01797@gmail.com>
2024-05-20 21:58:31 -0600

    Creacion de componentes list y form, tambien se crean las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tareas', component: TaskListComponent },
+  { path: 'tareas/formulario', component: TaskFormComponent },
+  { path: 'tareas/formulario/:id', component: TaskFormComponent },
+  { path: '**', redirectTo: 'tareas' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
